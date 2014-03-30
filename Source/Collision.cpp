5dc79ef0735f5ae8919e6c3b14495d6cc0f4ba13
jhpy1024 <jhpy1024@gmail.com>
2014-03-30 17:51:17 +0100

    Fixed circle vs circle collision detection and started rect vs rect collision detection

--- Source/Collision.cpp
+++ Source/Collision.cpp
@@ -22,6 +22,18 @@ bool Collision::rectangleVsRectangle(const Shape& shape1, const Shape& shape2)
 {
 	auto rect1 = static_cast<const RectangleShape&>(shape1);
 	auto rect2 = static_cast<const RectangleShape&>(shape2);
+
+	auto rect1Top = rect1.position.y + rect1.height / 2.f;
+	auto rect1Bottom = rect1.position.y - rect1.height / 2.f;
+	auto rect1Left = rect1.position.x - rect1.width / 2.f;
+	auto rect1Right = rect1.position.x + rect1.width / 2.f;
+
+	auto rect2Top = rect2.position.y + rect2.height / 2.f;
+	auto rect2Bottom = rect2.position.y - rect2.height / 2.f;
+	auto rect2Left = rect2.position.x - rect2.width / 2.f;
+	auto rect2Right = rect2.position.x + rect2.width / 2.f;
+
+	return !((rect1Bottom < rect2Top) || (rect1Top > rect2Bottom) || (rect1Left > rect2Right) || (rect1Right < rect2Left));
 }
 
 bool Collision::circleVsCircle(const Shape& shape1, const Shape& shape2)
@@ -31,8 +43,8 @@ bool Collision::circleVsCircle(const Shape& shape1, const Shape& shape2)
 
 	auto pos1 = shape1.position;
 	auto pos2 = shape2.position;
-	auto distanceSquared = ((pos1.x + pos2.x) * (pos1.x + pos2.x)) * ((pos1.y + pos2.y) * (pos1.y + pos2.y));
-	auto sumRadiusSquared = (circle1.radius + circle2.radius) * (circle1.radius + circle2.radius);
+	auto distanceSquared = ((pos1.x + pos2.x) * (pos1.x + pos2.x)) + ((pos1.y + pos2.y) * (pos1.y + pos2.y));
+	auto sumRadiiSquared = (circle1.radius + circle2.radius) * (circle1.radius + circle2.radius);
 
-	return distanceSquared <= sumRadiusSquared;
+	return distanceSquared <= sumRadiiSquared;
 }
