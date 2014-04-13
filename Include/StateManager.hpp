7a591ba34b7a7d87f50e5f1b52b22d4fc36f9cdb
jhpy1024 <jhpy1024@gmail.com>
2014-04-13 20:59:38 +0100

    Created State Manager

---
+++ Include/StateManager.hpp
@@ -0,0 +1,30 @@
+#ifndef STATE_MANAGER_HPP
+#define STATE_MANAGER_HPP
+
+#include "State.hpp"
+
+#include <stack>
+#include <memory>
+
+class StateManager
+{
+public:
+	static StateManager& getInstance();
+
+	void push(std::shared_ptr<State> state);
+	void pop();
+
+	std::shared_ptr<State> top();
+
+	bool empty() const;
+
+private:
+	StateManager() = default;
+
+private:
+	static std::unique_ptr<StateManager> m_Instance;
+
+	std::stack<std::shared_ptr<State>> m_States;
+};
+
+#endif
---
+++ Source/StateManager.cpp
@@ -0,0 +1,31 @@
+#include "StateManager.hpp"
+
+std::unique_ptr<StateManager> StateManager::m_Instance;
+
+StateManager& StateManager::getInstance()
+{
+	if (m_Instance == nullptr)
+		m_Instance = std::unique_ptr<StateManager>(new StateManager);
+
+	return *m_Instance;
+}
+
+void StateManager::push(std::shared_ptr<State> state)
+{
+	m_States.push(state);
+}
+
+void StateManager::pop()
+{
+	m_States.pop();
+}
+
+std::shared_ptr<State> StateManager::top()
+{
+	return m_States.top();
+}
+
+bool StateManager::empty() const
+{
+	return m_States.empty();
+}
